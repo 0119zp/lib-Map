39727a912c4c627b4ee01a14f9f6b3167e95481c
张攀攀 <zpzibo0119@163.com>
2019-04-11 14:36:12 +0800

    定位调用修正

--- app/src/main/java/zp/com/baidumapdemo/MainActivity.java
+++ app/src/main/java/zp/com/baidumapdemo/MainActivity.java
@@ -1,8 +1,8 @@
 package zp.com.baidumapdemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import zp.com.baidulocation.baidulocation.BaiduLocationManager;
+import android.support.v7.app.AppCompatActivity;
+import zp.com.baidulocation.LocationLibManager;
 import zp.com.baidulocation.bean.LocationFailedBean;
 import zp.com.baidulocation.bean.LocationSuccessBean;
 import zp.com.baidulocation.callback.GpsLocationCallback;
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void initLocation() {
 
-        new BaiduLocationManager().getLocation(this, new GpsLocationCallback() {
+        LocationLibManager.getInstance().getLocation(this, new GpsLocationCallback() {
             @Override
             public void success(LocationSuccessBean response) {
                 // 定位成功
